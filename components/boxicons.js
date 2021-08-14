0f67d7e04853ca63633c573e42ebaa922d2d83ab
SlopeGeek <machida-s14@c.kyoai.ac.jp>
2021-08-14 23:48:39 +0900

    boxiconsを使用できるコンポーネントを作成

---
+++ components/boxicons.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Boxicons() {
+    return(
+        <div>
+            <Head>
+            <link href='https://unpkg.com/boxicons@2.0.8/css/boxicons.min.css' rel='stylesheet'></link>
+            <script src="https://unpkg.com/boxicons@2.0.8/dist/boxicons.js"></script>
+            </Head>
+        </div>
+    )
+}
