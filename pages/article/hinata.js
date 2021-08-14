ad5912ffb0428c6e20238a98b9275a19e00a3f89
SlopeGeek <machida-s14@c.kyoai.ac.jp>
2021-08-14 23:47:33 +0900

    Memberページにスライドバーver2を付与

--- pages/article/hinata.js
+++ pages/article/hinata.js
@@ -2,6 +2,9 @@ import Parser from 'rss-parser'
 import Head from 'next/head'
 import { useEffect } from 'react'
 import Slidebar1 from '../../components/organisms/slidebar_1'
+import Slidebar2 from '../../components/organisms/slidebar_2'
+import CSSModuleTest from '../../components/organisms/CSSModuleTest'
+import UserCards from '../../components/organisms/usercards'
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 const parser = new Parser();
 const url = CORS_PROXY + "http://ino.xrea.jp/hinatazaka46/?id=18"
@@ -10,6 +13,9 @@ const sampleurl = CORS_PROXY + "https://zenn.dev/hiro/feed"
 
 export default function Hinata() {
     return (
-        <Slidebar1></Slidebar1>
+        <div>
+            <UserCards></UserCards>
+            <Slidebar2></Slidebar2>
+        </div>
     )
 }
